1904c8de8fa2f6aed7f75f5f7e63097ed5a2d086
Tino <calvin12968452@gmail.com>
2018-08-07 17:28:11 +0800

    db 連結 以及EJS render enginee &path &routes &mysql driver套件安裝 成功撈DB資料

--- file_server.js
+++ file_server.js
@@ -1,60 +1,85 @@
 var express = require('express');
 var app = express(); //產生 Express Application 物件
+var path = require('path');
+var mysql = require('mysql');
+var routes = require('routes');
+
+//var users = require('./routes/users');
+
+
 app.use('/template',function(req, res, next){
   console.log(req,url);
   next();
 } );
+//lets css work
 app.use('/css',express.static('css'));
+
 app.get('/', function(req, res) {  //當連線到跟目錄的時候跳出以下訊息
   res.sendFile(__dirname + '/template/homepage/homepage.html');
   
 });
 
-
+//route
 app.get('/normal',function(req, res){
   res.sendFile(__dirname + '/template/normal/normal.html');
 });
 
+app.get('/vendor',function(req, res){
+  res.sendFile(__dirname + '/template/businesspage/vendor.html');
+});
+
+
+app.get('/signup',function(req, res){
+  res.sendFile(__dirname + '/template/signup/signup.html');
+});
+
+//let manager konws that server is working
 app.listen(8080, function(){
   console.log("伺服器已經啟動在 http://localhost:8080/");
 });
 
 
 
-/*
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var adr = 'http://localhost:8080/template/homepage/homepage.html';
-var q = url.parse(adr, true);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
 
-var express = require('express');
-var app = express(); //產生 Express Application 物件
-const hostname = '127.0.0.1';
-const port = 8080;
-app.get('/', function(req, res) {  //當連線到跟目錄的時候跳出以下訊息
-    res.send('hello <h>world</h>');
+
+//DB 
+
+//db state
+app.use(function(req, res, next){
+  req.config = config;
+  next();
+})
+
+//config for db
+var config = mysql.createConnection({
+  host: "localhost",
+  user:"root",
+  password:"",
+  server:"localhost\\SQLEXPRESS",   //這邊要注意一下!!
+  database:"mumo"
 });
-app.listen(8080, function(){
-    console.log("伺服器已經啟動在 http://localhost:8080/");
+
+//coonect to db
+config.connect(function(err){
+  if(err) {
+    console.log("connecting error");
+    return ;
+  }
+  console.log ("connecting success");
 });
 
+app.get('/test', function(req, res, next){
+  var db = req.config;
+  var data = "";
 
-//if not found page else deplay page
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end('Hello my <u>first server</u>\n');
+  db.query('SELECT * FROM user', function(err, rows){
+    if(err)
+      console.log(err);
+    var data = rows;
+    res.render('test',{title:"use infor",data: data});
   });
-}).listen(8080); 
-*/
+});

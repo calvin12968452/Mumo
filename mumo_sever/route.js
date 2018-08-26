951e043b892e771e3c6d6c846abe84e760b7e117
GivonQQ <s104213055@mail1.ncnu.edu.tw>
2018-08-26 17:44:29 +0800

    20180826

---
+++ mumo_sever/route.js
@@ -0,0 +1,45 @@
+const BodyParser = require('body-parser');
+// ====================================
+// colorful console in terminal =======
+// ====================================
+const {
+  textStyle,
+  textFg,
+  textBg
+} = require('./module/color.js');
+
+module.exports = function(app, ls, passport){
+
+  // ====================================
+  // routing setting ====================
+  // ====================================
+  app.use(BodyParser.urlencoded({ extended: false }));
+  app.use(BodyParser.json());
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // ====================================
+  // set the route ======================
+  // ====================================
+  app.get('/', function(req, res){    // <-- home page
+    res.sendFile('./views/HomeScreen.html', {root: __dirname});
+    ls.set('google_user', null);
+    ls.set('fb_user', null);
+  });
+
+  app.get('/profile', function(req, res){   // <-- logined page
+    res.render('C:\\Users\\user\\Desktop\\Gcloud\\gcloud\\views\\component\\profile.ejs', { google_user: ls.get('google_user'), fb_user: ls.get('fb_user') });
+  });
+
+  // google login route
+  app.get('/oauth', passport.authenticate('google_token', {scope : ['profile', 'email']}));
+
+  // google login successfully then callback to the route
+  app.get('/oauth/callback', passport.authenticate('google_token', {successRedirect: '/profile', failureRedirect: '/'}));
+
+  // fb login route
+  app.get('/fb', passport.authenticate('fb_token', { authType: 'rerequest', scope: ['user_friends', 'manage_pages'] }));
+
+  // fb login successfully then callback to the route
+  app.get('/fb/callback', passport.authenticate('fb_token', {successRedirect: '/profile', failureRedirect: '/'}));
+}

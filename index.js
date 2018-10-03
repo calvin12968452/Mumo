2b415322789808bf6aa34333aaf74b064b93292e
GivonQQ <s104213055@mail1.ncnu.edu.tw>
2018-10-03 22:50:39 +0800

    db初始連結

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var http = require("http").Server(app);
 var io = require('socket.io')(http);
 var ls = require('local-storage');    // <-- local storage, can be changed to your database. hint : mongoose
 // useage：ls.get('ls-name')
+var mysql = require ('mysql');
 
 // ====================================
 // colorful console in terminal =======
@@ -18,9 +19,17 @@ const {
 var imp_passport = require('./controller/passport.js');
 var Route = require('./route.js');
 var chat = require('./controller/chatroom.js');
+var db_connect = require('./sql/dbMYSQL.js');
+var db = mysql.creatConnection ({
+  host:"localhost",
+  user:"root",
+  password:""
+});
+
+db_connect(db);
 
 chat(io);
-imp_passport(ls, passport);   // <-- implement the passport method
+imp_passport(ls, passport, db);   // <-- implement the passport method
 Route(app, express, ls, passport);     // <-- implement the routing setting
 
 
